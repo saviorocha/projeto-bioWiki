5ebbc07988b86b5bed52a80a9c255193a7221976
saviorocha <44814299+saviorocha@users.noreply.github.com>
2020-05-23 15:53:03 -0300

    Adicionada lista de habilidades

--- backend/controllers/habilidadeController.js
+++ backend/controllers/habilidadeController.js
@@ -1,33 +1,37 @@
 let Habilidade = require('../models/habilidade');
 
-exports.habilidade_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: habilidade_list');
+exports.habilidade_list = function (req, res) {
+    Habilidade.find({}, 'nome')
+        .exec(function (err, list_habilidade) {
+            if (err) { return next(err); }
+            res.render('habilidade_list', {title: 'Lista de habilidades', habilidade_list: list_habilidade});
+        });
 }
 
-exports.habilidade_detail = function(req, res) {
+exports.habilidade_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: habilidade_detail: ' + req.params.id);
 }
 
-exports.habilidade_create_get = function(req, res) {
+exports.habilidade_create_get = function (req, res) {
     res.send('NOT IMPLEMENTED: habilidade_create_get');
 }
 
-exports.habilidade_create_post = function(req, res) {
+exports.habilidade_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: habilidade_create_post');
 }
 
-exports.habilidade_update_get = function(req, res) {
+exports.habilidade_update_get = function (req, res) {
     res.send('NOT IMPLEMENTED: habilidade_update_get');
 }
 
-exports.habilidade_update_post = function(req, res) {
+exports.habilidade_update_post = function (req, res) {
     res.send('NOT IMPLEMENTED: habilidade_update_post');
 }
 
-exports.habilidade_delete_get = function(req, res) {
+exports.habilidade_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: habilidade_delete_get');
 }
 
-exports.habilidade_delete_post = function(req, res) {
+exports.habilidade_delete_post = function (req, res) {
     res.send('NOT IMPLEMENTED: habilidade_delete_post');
 }
--- backend/models/habilidade.js
+++ backend/models/habilidade.js
@@ -5,7 +5,7 @@ let Schema = mongoose.Schema;
 let HabilidadeSchema = new Schema({
     nome: { type: String, max: 100, required: true, unique: true },
     descricao: { type: String, max: 500, required: true },
-    tipo: { type: String, enum: ['Ataque', 'Defesa', 'Suporte'], max: 50, required: true }
+    tipo: { type: String, enum: ['Ataque', 'Defesa', 'Suporte', 'Outro'], max: 50, required: true }
 });
 
 HabilidadeSchema
